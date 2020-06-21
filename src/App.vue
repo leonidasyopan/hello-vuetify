8e2af3f938cbbb50267ccb21065b83410f8d5a2e
Leonidas Yopan <leonidasyopan@gmail.com>
2020-06-20 22:28:47 -0300

    edditing the wrong files

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,7 @@
 <template lang="pug">
-  v-row
-    v-col(
-      v-for="(bar, i) in bars"
-      :key="i"
-      cols="12"
-      sm="12"
-      md="6"
-      class="my-4"
-    )
-    
-      v-card(color="grey lighten-4" flat height="200px")
-        v-toolbar(:color="bar.class" :dark="bar.dark")
-          v-app-bar-nav-icon
-          v-toolbar-title
-          v-spacer
-          v-btn icon
-            v-icon
-          
-          v-btn icon
-            v-icon
-          
-          v-btn icon
-            v-icon
+  v-app
+    v-content
+      router-view
 </template>
 
 <script>
